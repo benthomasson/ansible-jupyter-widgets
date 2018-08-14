48f04d4e32fa39312853d368a43b2141e4e27d08
Ben Thomasson <bthomass@redhat.com>
2018-08-14 16:28:40 -0400

    Add var_name arg to hello world

--- action_plugins/hello_world.py
+++ action_plugins/hello_world.py
@@ -10,7 +10,9 @@ class ActionModule(ActionBase):
             task_vars = dict()
         result = super(ActionModule, self).run(tmp, task_vars)
         var_name = self._task.args.get('var_name', None)
+        if var_name is None:
+            raise Exception("'var_name' is a required argument")
         result['application/x-python'] = """import jupyter_widget_example
-{0} = jupyter_widget_example.HelloWorld()
+{0} = jupyter_widget_example.HelloWorld(var_name='{0}')
 display({0})""".format(var_name)
         return result
