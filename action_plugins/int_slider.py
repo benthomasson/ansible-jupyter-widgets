749201cce9f99ac0bd0600076a37d36198df21d1
Ben Thomasson <bthomass@redhat.com>
2018-08-15 13:39:06 -0400

    Add widgets to the role directly

---
+++ action_plugins/int_slider.py
@@ -0,0 +1,19 @@
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+
+    BYPASS_HOST_LOOP = True
+
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = dict()
+        result = super(ActionModule, self).run(tmp, task_vars)
+        var_name = self._task.args.get('var_name', None)
+        description = self._task.args.get('description', var_name)
+        if var_name is None:
+            raise Exception("'var_name' is a required argument")
+        result['application/x-python'] = """import ansible_jupyter_widgets
+{0} = ansible_jupyter_widgets.IntSlider(var_name='{0}', description='{1}')
+display({0})""".format(var_name, description)
+        return result
